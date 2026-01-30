65753993b652c267963f1513b3eddd94ce2df695
DONGHYUN_PC\xlr3z <dhmoon1023@gmail.com>
2026-01-30 23:36:13 +0900

    Home 화면 기능 개발

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,21 @@ import NotFound from "./pages/NotFound"
 const MockData = [
     {
         id : 1,
-        createdDate : new Date().getTime(),
+        createdDate : new Date("2026-01-30").getTime(),
         emotionId : 1,
         content : "1번 일기 내용"
     },
     {
         id : 2,
-        createdDate : new Date().getTime(),
+        createdDate : new Date("2026-01-29").getTime(),
         emotionId : 2,
         content : "2번 일기 내용"
+    },
+    {
+        id : 3,
+        createdDate : new Date("2025-12-25").getTime(),
+        emotionId : 3,
+        content : "3번 일기 내용"
     }
 ]
 
@@ -39,8 +45,8 @@ function reducer(state, action) {
     }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 
 function App() {
