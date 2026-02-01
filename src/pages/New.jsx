b008c75d2f6db4355736c17d5c288cd9d6537cd6
DONGHYUN_PC\xlr3z <dhmoon1023@gmail.com>
2026-02-01 20:50:15 +0900

    New 페이지 UI 작업

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,6 +1,17 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const New = () => {
-    return <div>New</div>
+    return (
+        <div className="New">
+            <Header 
+                title={"새 일기 쓰기"} 
+                leftChild={<Button text={"<뒤로가기"} />}
+            />
+            <Editor />
+        </div>
+    )
 }
 
 export default New;
